9538d7bbb84d9549de979b7ae3777b60d2dbabb9
Andrés Liu <andresdavidliu@hotmail.com>
2018-03-17 11:40:53 -0500

    Updated portfolio
    Updated portfolio

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 //@prepros-prepend "./vendors/jquery.paroller.min.js;
 //@prepros-prepend "./vendors/jquery.scrollTo.min.js;
-//@prepros-prepend "./modules/loader.js";
 //@prepros-prepend "./modules/hero.js";
 //@prepros-prepend "./modules/portfolio.js";
 
-const loader = new Loader();
 const hero = new Hero();
 const portfolio = new Portfolio();
